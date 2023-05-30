85676eb74d5eee9c9acc52db524b4baa41c8683e
Ian Coto <iancotosoto@gmail.com>
2023-05-30 10:26:19 -0600

    Prueba de todos y sirven

--- Progra 2/src/ServerClient/App.java
+++ Progra 2/src/ServerClient/App.java
@@ -2,12 +2,19 @@ package ServerClient;
 
 import Cocina.ControlerCocina.ControladorCocina;
 import ControlerSalon.controlerSalon;
+import Simulacion.Controler.ControlerSimulacion;
 
 public class App {
     public static void main(String[] args) throws Exception{
+
+        // Simulacion
+        ControlerSimulacion simulacion = new ControlerSimulacion();
+        simulacion.start();
+
         // salon
         controlerSalon controler = new controlerSalon();
         ServerSalon servidorSalon = new ServerSalon(controler); // servidor para recibir ordenes listas de la cocina
+        server servidoServer = new server(controler.modelSalon);
 
         //cocina
         ControladorCocina controlador = new ControladorCocina();
